43870104c0bff085b24630afdab2a232a077f5b7
Stephen <smatheny@ncscredit.com>
2024-06-10 18:42:57 -0500

    update colors to sky

--- resources/js/Components/SecondaryButton.jsx
+++ resources/js/Components/SecondaryButton.jsx
@@ -4,7 +4,7 @@ export default function SecondaryButton({ type = 'button', className = '', disab
             {...props}
             type={type}
             className={
-                `inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 ${
+                `inline-flex items-center px-4 py-2 bg-white dark:bg-sky-800 border border-sky-300 dark:border-sky-500 rounded-md font-semibold text-xs text-sky-700 dark:text-sky-300 uppercase tracking-widest shadow-sm hover:bg-sky-50 dark:hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-sky-800 disabled:opacity-25 transition ease-in-out duration-150 ${
                     disabled && 'opacity-25'
                 } ` + className
             }
