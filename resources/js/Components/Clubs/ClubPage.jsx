a0789a144a28edbf609ad95d19f02608aef3794b
Stephen <smatheny@ncscredit.com>
2024-06-14 16:22:47 -0500

    more club edits

--- resources/js/Components/Clubs/ClubPage.jsx
+++ resources/js/Components/Clubs/ClubPage.jsx
@@ -1,16 +1,36 @@
 import { Link } from "@inertiajs/react";
 
 export default function ClubPage({ club, users }) {
+    const coordinators = users.data.filter(user => user.pivot.role === 'admin');
     return (
-        <div className="max-w-full sm:max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4">
-            <div className="flex flex-col mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg">
-                <div className="p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
-                    {club.name}
-                </div>
-                <div className="p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
-                    {users.data.map(user => {
-                        return <p>{user.first_name} {user.last_name}</p>
-                    })}
+        <div className="max-w-full sm:max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 block">
+            <div className="mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg">
+                <div className="flex flex-col p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
+                    {coordinators.length > 0 && (
+                        <div className="mt-2">
+                            Coordinators: {coordinators.map((coordinator, index) => (
+                                <span key={coordinator.id}>
+                                    <Link className="text-sm text-cyan-400  hover:duration-300 hover:text-cyan-600" href="#">
+                                        {coordinator.first_name} {coordinator.last_name}
+                                    </Link>
+                                    {index < coordinators.length - 1 && ', '}
+                                </span>
+                            ))}
+                        </div>
+                    )}
+
+                    {users.data.length > 0 && (
+                        <div className="mt-2">
+                            Members: {users.data.map((user, index) => (
+                                <span key={user.id}>
+                                    <Link className="text-sm text-cyan-400 hover:duration-300 hover:text-cyan-600" href={route('user.show', { id: user.id })}>
+                                        {user.first_name} {user.last_name}
+                                    </Link>
+                                    {index < users.data.length - 1 ? ', ' : '...'}
+                                </span>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- resources/js/Pages/Clubs/Show.jsx
+++ resources/js/Pages/Clubs/Show.jsx
@@ -1,6 +1,6 @@
 import ClubPage from "@/Components/Clubs/ClubPage";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 
 
@@ -9,9 +9,16 @@ export default function Show({ auth, club, users }) {
         <>
             <AuthenticatedLayout
                 user={auth.user}
-                header={<h2 className="font-semibold text-xl text-cyan-800 dark:text-logoColor leading-tight">Clubs</h2>}
+                header={<>
+                    <h2 className="font-semibold text-xl text-cyan-800 dark:text-logoColor leading-tight">
+                        {club.name}
+                    </h2>
+                    <Link className="mt-auto text-sm text-cyan-400 hover:duration-300 hover:text-cyan-600" href="#">
+                        {club.city}, {club.state}
+                    </Link>
+                </>}
             >
-                <Head title="Clubs" />
+                <Head title={club.name} />
 
                 <div className="py-12">
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
