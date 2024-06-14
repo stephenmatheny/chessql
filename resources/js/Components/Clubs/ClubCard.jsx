ce48207a9f7016064a62dc447ae5017f0fd8ef77
Stephen <smatheny@ncscredit.com>
2024-06-13 23:35:13 -0500

    update club page

--- resources/js/Components/Clubs/ClubCard.jsx
+++ resources/js/Components/Clubs/ClubCard.jsx
@@ -2,14 +2,12 @@ import { Link } from "@inertiajs/react";
 
 export default function ClubCard({ club }) {
     return (
-        <div className="max-w-full sm:max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4">
-            <div className="mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg">
+        <Link href={route('clubs.show', { id: club.id })} className="max-w-full sm:max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 block">
+            <div className="mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg transform hover:scale-105">
                 <div className="p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
-                    <Link href={route('clubs.show', { id: club.id })}>
-                        {club.name}
-                    </Link>
+                    {club.name}
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
--- resources/js/Components/Clubs/ClubPage.jsx
+++ resources/js/Components/Clubs/ClubPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "@inertiajs/react";
 
 export default function ClubPage({ club, users }) {
-    console.log(users);
     return (
         <div className="max-w-full sm:max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4">
-            <div className="mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg">
+            <div className="flex flex-col mx-4 bg-white dark:bg-cyan-800 overflow-hidden shadow-sm rounded-lg">
                 <div className="p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
                     {club.name}
+                </div>
+                <div className="p-4 sm:p-6 text-cyan-900 dark:text-cyan-100">
+                    {users.data.map(user => {
+                        return <p>{user.first_name} {user.last_name}</p>
+                    })}
                 </div>
             </div>
         </div>
